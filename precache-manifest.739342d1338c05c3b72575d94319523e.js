4509061f8a8fa6b51c5e4baf8267809982e604e2
highhack <forgreatperson@gmail.com>
2021-03-01 13:48:08 +0200

    Updates

---
+++ precache-manifest.739342d1338c05c3b72575d94319523e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3112ee65c67f4c049bc20e4d27f8eebf",
+    "url": "/TodoLIst/index.html"
+  },
+  {
+    "revision": "6b1b30914080c3a1a9f2",
+    "url": "/TodoLIst/static/css/main.ac787a0f.chunk.css"
+  },
+  {
+    "revision": "ab1e03a32bf9843cac1b",
+    "url": "/TodoLIst/static/js/2.924014ea.chunk.js"
+  },
+  {
+    "revision": "92bfebbc9186106791d506e48cb876d1",
+    "url": "/TodoLIst/static/js/2.924014ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b1b30914080c3a1a9f2",
+    "url": "/TodoLIst/static/js/main.fd8f2782.chunk.js"
+  },
+  {
+    "revision": "4d205bd12934bcf33582",
+    "url": "/TodoLIst/static/js/runtime-main.63684b4b.js"
+  }
+]);
